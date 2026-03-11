24112df5660b0b08f236489c99e2cc3f5796442b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-10 22:48:43 -0300

    Terminamos o exercicio 03, foi um sistema basico de bilhete unico, utilizando carregar, verificar saldo, passar na catraca e sair

--- src/Exercicio03/BilheUnico.java
+++ src/Exercicio03/BilheUnico.java
@@ -7,20 +7,20 @@ public class BilheUnico {
     int numero;
     String usuario;
     double saldo;
-    final static double valorBase=5.50;
+    final static double valorBase=5.40;
     String tipoTarifa;
 
 
-    public BilheUnico(String usuario,String tipoTarifa, double saldo){
+    public BilheUnico(String usuario,String tipoTarifa){
         Random rd= new Random(10000);
 
         this.numero = rd.nextInt(0, 999999);
         this.usuario=usuario;
         this.tipoTarifa=tipoTarifa;
-        this. saldo= saldo;
+
     }
 
-    public void passarNaCatraca(){
+    public boolean passarNaCatraca(){
         double valor= valorBase;
         if (tipoTarifa.equalsIgnoreCase("professor") || tipoTarifa.equalsIgnoreCase("estudante")){
 
@@ -28,12 +28,16 @@ public class BilheUnico {
 
         }
         if (saldo < valor){
-
             System.out.println("VC É INUTIL, VC NÃO TEM DINHEIRO, VC DEU PRESENTE PRA NAMORA, EU TE ENTENDO, " +
                     "CASO VC NAO TENHA NAMORA, ENTÃO VC TEM DINHEIRO, RECARREGUE, SE MATE!");
-            return;
+
+            return false;
         }
-        saldo-=valor;
+
+        else {
+            saldo-=valor;
+            return true;
+        }
 
     }
 
--- src/Exercicio03/Main.java
+++ src/Exercicio03/Main.java
@@ -1,9 +1,65 @@
 package Exercicio03;
-
-import Exercicio02.SenhaAtendimento;
+import java.util.Scanner;
 
 public class Main {
+
+        static Scanner sc = new Scanner(System.in);
+        static BilheUnico bilheUnico;
+        static{
+        System.out.println("Qual é o seu nome--> ");
+        String nome= sc.next();
+
+        System.out.println("Qual é o tipo de tarifa(estudante ou professor)--> ");
+        String tipoTarifa= sc.next();
+
+        bilheUnico= new BilheUnico(nome, tipoTarifa);
+    }
+
     public static void main(String[] args) {
 
+        int respostas;
+
+
+        do {
+            System.out.println("1. Carregar Bilhete" + "\n" + "2. Consultar Saldo" + "\n" + "3. Passar na catraca" + "\n" + "4. Sair");
+            respostas=sc.nextInt();
+            switch (respostas) {
+                case (1) -> carregar();
+
+                case (2) -> consultar();
+
+                case (3) -> catraca();
+
+                case (4) ->
+                        System.out.println("Até logo");
+            }
+        }
+        while (respostas != 4);
+
+        System.out.println();
+
     }
+
+    private static void catraca() {
+        if (bilheUnico.passarNaCatraca()){
+
+        }
+    }
+
+    private static void consultar() {
+
+        System.out.println("Saldo atual R$" + bilheUnico.saldo);
+
+    }
+
+    public static void carregar(){
+        System.out.println("Quanto você deseja carregar");
+        double valor=sc.nextDouble();
+
+        bilheUnico.saldo+= valor;
+    }
+
+
+
+
 }
